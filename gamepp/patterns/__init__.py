26a93a31d211ac5945c90a0ac642223b1e613e5f
BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>
2025-05-16 19:41:57 -0500

    Update README, include missing patterns in __all__

--- gamepp/patterns/__init__.py
+++ gamepp/patterns/__init__.py
@@ -8,27 +8,53 @@ from .double_buffer import Buffer, DoubleBuffer
 from .update_method import UpdateMethodManager, Entity
 from .bytecode import Instruction, VirtualMachine
 from .service_locator import ServiceLocator, NullService, get_service, register_service
+from .command import Command
+from .component import Component, GameObject
+from .data_locality import GameEntity, PositionComponent, RenderComponent, AIComponent, PhysicsComponent, ComponentManager
+from .dirty_flag import DirtyFlag, Transform
+from .event_queue import EventQueue, Event
+from .flyweight import Flyweight, FlyweightFactory
+from .game_loop import GameLoop
+from .interpreter import (
+    Expression, TerminalExpression, NonTerminalExpression,
+    Number, Add, Subtract, Multiply, Divide, Parser
+)
+from .object_pool import ObjectPool, Reusable
+from .pda import PDA, State as PDAState, Transition as PDATransition
+from .spatial_partition import SpatialPartition, GameObject as SPGameObject
+from .type_object import Breed, Monster
 
 __all__ = [
     "Command",
-    "Flyweight",
+    "Component", "GameObject",
+    "GameEntity", "PositionComponent", "RenderComponent", "AIComponent", "PhysicsComponent", "ComponentManager",
+    "DirtyFlag", "Transform",
+    "EventQueue", "Event",
+    "Flyweight", "FlyweightFactory",
     "StateMachine",
     "State",
     "HStateMachine",
-    "Subject",
+    "GameLoop",
+    "Expression", "TerminalExpression", "NonTerminalExpression",
+    "Number", "Add", "Subtract", "Multiply", "Divide", "Parser",
+    "ObjectPool", "Reusable",
     "ObserverMixin",
+    "Subject",
+    "PDA", "PDAState", "PDATransition",
     "Prototype",
-    "Singleton",
-    "CSM",
-    "StateMachineInterface",
-    "Buffer",
-    "DoubleBuffer",
-    "UpdateMethodManager",
-    "Entity",
-    "Instruction",
-    "VirtualMachine",
     "ServiceLocator",
     "NullService",
     "get_service",
     "register_service",
+    "Singleton",
+    "SpatialPartition", "SPGameObject",
+    "CSM",
+    "StateMachineInterface",
+    "Buffer",
+    "DoubleBuffer",
+    "Breed", "Monster",
+    "UpdateMethodManager",
+    "Entity",
+    "Instruction",
+    "VirtualMachine",
 ]
